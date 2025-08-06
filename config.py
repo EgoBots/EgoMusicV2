a8c8e96c3faff1ed5784f80446dafae8232abd8a
×𝐄𝐆𝐎//𝐑𝐄𝐌𝐈𝐗/𝟑 <Shizubots@outlook.com>
2025-08-06 19:37:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,8 +36,8 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 # Support
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AarumiBots")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AarumiChat")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/RemixSuport")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+kjW2ZEyJ5ntmZGU1")
 
 # Assistant settings
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "True")
