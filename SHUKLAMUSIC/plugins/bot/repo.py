50bff905fffac0b3ae5ae73634132f6053a90609
×𝐄𝐆𝐎//𝐑𝐄𝐌𝐈𝐗/𝟑 <Shizubots@outlook.com>
2025-08-06 19:03:17 +0530

    Update repo.py

--- SHUKLAMUSIC/plugins/bot/repo.py
+++ SHUKLAMUSIC/plugins/bot/repo.py
@@ -6,12 +6,12 @@ from SHUKLAMUSIC import app
 from config import BOT_USERNAME
 
 # Caption Text
-start_txt = """<b>✨ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ <u>sᴛʀᴀɴɢᴇʀ ʀᴇᴘᴏs</u></b>
+start_txt = """<b>✨ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ <u>ᴧᴧʀᴜᴍɪ ʀᴇᴘᴏs</u></b>
 
-🚀 <b>ᴇᴀsʏ ᴅᴇᴘʟᴏʏ</b> –ᴏɴᴇ ᴄʟɪᴄᴋ ʜᴇʀᴏᴋᴜ ᴅᴇᴘʟᴏʏᴍᴇɴᴛ  
-🛡️ <b>ɴᴏ ʜᴇʀᴏᴋᴜ ᴏʀ ɪᴅ ʙᴀɴ ɪssᴜᴇs</b>  
-🔋 <b>ᴜɴʟɪᴍɪᴛᴇᴅ ᴅʏɴᴏs</b> – ʀᴜɴ 24/7 ʟᴀɢɢ-ғʀᴇᴇ  
-⚙️ <b>ғᴜʟʟʏ ғᴜɴᴄᴛɪᴏɴᴀʟ & ᴇʀʀᴏʀ-ғʀᴇᴇ</b>  
+● <b>ᴇᴧsʏ ᴅᴇᴘʟᴏʏ</b> –ᴏɴᴇ ᴄʟɪᴄᴋ ʜᴇʀᴏᴋᴜ ᴅᴇᴘʟᴏʏᴍᴇɴᴛ  
+● <b>ɴᴏ ʜᴇʀᴏᴋᴜ ᴏʀ ɪᴅ ʙᴧɴ ɪssᴜᴇs</b>  
+● <b>ᴜɴʟɪᴍɪᴛᴇᴅ ᴅʏɴᴏs</b> – ʀᴜɴ 24/7 ʟᴧɢɢ-ғʀᴇᴇ  
+● <b>ғᴜʟʟʏ ғᴜɴᴄᴛɪᴏɴᴧʟ & ᴇʀʀᴏʀ-ғʀᴇᴇ</b>  
 
 <i>ɴᴇᴇᴅ ʜᴇʟᴘ? sᴇɴᴅ sᴄʀᴇᴇɴsʜᴏᴛ ᴛᴏ ᴛʜᴇ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ!</i>"""
 
@@ -19,37 +19,37 @@ start_txt = """<b>✨ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ <u>sᴛʀᴀɴɢᴇʀ ʀᴇᴘ
 @app.on_message(filters.command("repo"))
 async def repo_handler(_, msg):
     buttons = [
-        [InlineKeyboardButton("➕ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
+        [InlineKeyboardButton("⌯ ᴧᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ʙᴧʙʏ  ⌯", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
         [
-            InlineKeyboardButton("💬 sᴜᴘᴘᴏʀᴛ", url="https://t.me/MASTIWITHFRIENDSXD"),
-            InlineKeyboardButton("👤 ᴏᴡɴᴇʀ", url="https://t.me/SHASHANKDEVS"),
+            InlineKeyboardButton("⌯ sᴜᴘᴘᴏʀᴛ ⌯", url="https://t.me/AarumiChat"),
+            InlineKeyboardButton("⌯ ᴏᴡɴᴇʀ ⌯", url="https://t.me/Swagger_Soul"),
         ],
-        [InlineKeyboardButton("🧾 ᴜᴘᴅᴀᴛᴇs", url="https://t.me/SHIVANSH474")],
+        [InlineKeyboardButton("⌯ ᴜᴘᴅᴧᴛᴇs ⌯", url="https://t.me/AarumiBots")],
         [
-            InlineKeyboardButton("💥 ʙᴀɴᴀʟʟ", url="https://github.com/itzshukla/STRANGER-BANALL/fork"),
-            InlineKeyboardButton("🎧 ᴍᴜsɪᴄ V2", url="https://github.com/itzshukla/STRANGER-MUSIC/fork"),
-        ],
-        [
-            InlineKeyboardButton("🎶 ᴍᴜsɪᴄ V1", url="https://github.com/itzshukla/STRANGER-MUSIC2.0/fork"),
-            InlineKeyboardButton("💬 ᴄʜᴀᴛʙᴏᴛ", url="https://github.com/itzshukla/STRANGER-CHATBOT/fork"),
+            InlineKeyboardButton("⌯ ʙᴧɴᴧʟʟ ⌯", url="https://github.com/itzshukla/STRANGER-BANALL/fork"),
+            InlineKeyboardButton("⌯ ᴍᴜsɪᴄ V2 ⌯", url="https://github.com/itzshukla/STRANGER-MUSIC/fork"),
         ],
         [
-            InlineKeyboardButton("🎯 sᴛʀɪɴɢ ɢᴇɴ", url="https://github.com/itzshukla/STRANGER-STRING-GEN/fork"),
-            InlineKeyboardButton("🛠️ ɢᴄ ᴍᴀɴᴀɢᴇʀ", url="https://github.com/itzshukla/STRANGER-ROBOT/fork"),
+            InlineKeyboardButton("⌯ ᴍᴜsɪᴄ V1 ⌯", url="https://github.com/itzshukla/STRANGER-MUSIC2.0/fork"),
+            InlineKeyboardButton("⌯ ᴄʜᴧᴛʙᴏᴛ ⌯", url="https://github.com/itzshukla/STRANGER-CHATBOT/fork"),
         ],
         [
-            InlineKeyboardButton("⚔️ sᴘᴀᴍ ʙᴏᴛs", url="https://github.com/itzshukla/STRANGER-SPAM-X/fork"),
-            InlineKeyboardButton("👾 ʙᴀɴᴀʟʟ 10", url="https://github.com/itzshukla/STRANGER-BANALL-BOTS/fork"),
+            InlineKeyboardButton("⌯ sᴛʀɪɴɢ ɢᴇɴ ⌯", url="https://github.com/itzshukla/STRANGER-STRING-GEN/fork"),
+            InlineKeyboardButton("⌯ ɢᴄ ᴍᴧɴᴧɢᴇʀ ⌯", url="https://github.com/itzshukla/STRANGER-ROBOT/fork"),
         ],
         [
-            InlineKeyboardButton("🧪 sᴛʀɪɴɢ ʜᴀᴄᴋ", url="https://github.com/itzshukla/STRANGER-SESSION-HACK/fork"),
-            InlineKeyboardButton("🤖 ɪᴅ ᴜsᴇʀʙᴏᴛ", url="https://t.me/StrangerHosterbot"),
+            InlineKeyboardButton("⌯ sᴘᴧᴍ ʙᴏᴛs ⌯", url="https://github.com/itzshukla/STRANGER-SPAM-X/fork"),
+            InlineKeyboardButton("⌯ ʙᴧɴᴧʟʟ 10 ⌯", url="https://github.com/itzshukla/STRANGER-BANALL-BOTS/fork"),
         ],
-        [InlineKeyboardButton("👑 sᴜᴘᴇʀ ᴜsᴇʀʙᴏᴛ", url="https://github.com/itzshukla/STRANGER-OPUSERBOT2.0/fork")]
+        [
+            InlineKeyboardButton("⌯ sᴛʀɪɴɢ ʜᴧᴄᴋ ⌯", url="https://github.com/itzshukla/STRANGER-SESSION-HACK/fork"),
+            InlineKeyboardButton("⌯ ɪᴅ ᴜsᴇʀʙᴏᴛ ⌯", url="https://t.me/StrangerHosterbot"),
+        ],
+        [InlineKeyboardButton("⌯ sᴜᴘᴇʀ ᴜsᴇʀʙᴏᴛ ⌯", url="https://github.com/itzshukla/STRANGER-OPUSERBOT2.0/fork")]
     ]
 
     await msg.reply_photo(
-        photo="https://files.catbox.moe/jxribw.jpg",
+        photo="https://files.catbox.moe/783eei.jpg",
         caption=start_txt,
         reply_markup=InlineKeyboardMarkup(buttons)
     )
@@ -72,7 +72,7 @@ async def repo(_, message):
             list_of_users += f"{count}. [{user['login']}]({user['html_url']})\n"
             count += 1
 
-        text = f"""[𝖱𝖤𝖯𝖮 𝖫𝖨𝖭𝖪](https://github.com/itzshukla/STRANGER-MUSIC) | [UPDATES](https://t.me/SHIVANSH474)
+        text = f"""[𝖱𝖤𝖯𝖮 𝖫𝖨𝖭𝖪](https://github.com/itzshukla/STRANGER-MUSIC) | [UPDATES](https://t.me/AarumiBots)
 | 𝖢𝖮𝖭𝖳𝖱𝖨𝖡𝖴𝖳𝖮𝖱𝖲 |
 ----------------
 {list_of_users}"""
